ecf3dfccf4880787de42b20ff9f2de7fe7a3fec8
Lilac <lilac@quartz.video>
2018-03-24 00:43:30 +1000

    add error ranges specified in newznab spec

--- nerrors.go
+++ nerrors.go
@@ -1,5 +1,7 @@
 package newznab
 
+import "fmt"
+
 // NError describes an error defined by the newznab specification
 type NError struct {
 	Code        int
@@ -47,4 +49,62 @@ var nerrors = map[int]NError{
 	300: ErrNoSuchItem,
 	900: ErrUnknownError,
 	910: ErrAPIDisabled,
+}
+
+// NErrorRange describes an error range defined by the newznab spec
+// it is used for when we do not know the meaning of the specific error code,
+// but can work out its general category from the range it falls into
+type NErrorRange struct {
+	// the lower bound of the error range
+	Min int
+	// the upper bound of the error range
+	Max int
+	// the description of the error range
+	Description string
+	// the code of the actual unknown error
+	Code int
+}
+
+// Error implements the error interface for the NErorrRange type
+func (n NErrorRange) Error() string {
+	if n.Code != 0 {
+		if !n.codeWithin(n.Code) {
+			return fmt.Sprintf("unknown newznab error")
+		}
+		return fmt.Sprintf("unknown newznab error %d: %s", n.Code, n.Description)
+	}
+	return fmt.Sprintf("unknown newznab error: %s", n.Description)
+}
+
+// withCode returns a copy of the called-on NErrorRange, with its Code field
+// set to the provided value
+func (n NErrorRange) withCode(code int) NErrorRange {
+	n.Code = code
+	return n
+}
+
+// codeWithin returns whether an arbitrary error code is within the range
+// defined in a NErrorRange
+func (n NErrorRange) codeWithin(code int) bool {
+	return n.Code >= n.Min && n.Code <= n.Max
+}
+
+// error ranges defined within the newznab spec
+var (
+	ErrUnspecifiedAccountOrUserCredentials = NErrorRange{Min: 100, Max: 199, Description: "issue with user credentials or account"}
+	ErrUnspecifiedAPICall                  = NErrorRange{Min: 200, Max: 299, Description: "API-call specific error"}
+	ErrUnspecifiedContent                  = NErrorRange{Min: 300, Max: 399, Description: "issue with content"}
+	ErrUnspecifiedOther                    = NErrorRange{Min: 900, Max: 999, Description: "other error"}
+)
+
+// nerrorRangeFromCode takes the code of an unknown error, and returns the
+// corresponding NErrorRange
+func nerrorRangeFromCode(code int) NErrorRange {
+	ranges := []NErrorRange{ErrUnspecifiedAccountOrUserCredentials, ErrUnspecifiedAPICall, ErrUnspecifiedContent, ErrUnspecifiedOther}
+	for _, ner := range ranges {
+		if ner.codeWithin(code) {
+			return ner
+		}
+	}
+	return ErrUnspecifiedOther
 }
